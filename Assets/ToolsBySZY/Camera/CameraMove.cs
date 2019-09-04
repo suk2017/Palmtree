965279d6ed2047a8eaa79152cac957bf7296c04d
Sunzuyu <sunzuyu2016@foxmail.com>
2019-09-04 16:17:04 +0800

    sad
    好像是还没备份到git 就因为版本更新丢失了 太诡异了 可能是在别的地方 可能是在另一个项目里

---
+++ Assets/ToolsBySZY/Camera/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    public GameObject Target;//追踪目标 主角
+    public float Speed = 0.2f;//追踪速度
+
+    private Transform tr;//自己的位置
+    private Transform target;//目标的位置
+
+    void Awake()
+    {
+        tr = this.transform;
+        target = Target.transform;
+    }
+
+    void Update()
+    {
+        float x = Mathf.Lerp(tr.position.x, target.position.x, Speed);
+        tr.position = new Vector3(x, 0, -10);
+    }
+}
